c6ef919c96ee73c172fcd190d2fc38950d50633b
glitvjak <gatislitvjaks@gmail.com>
2023-08-27 13:29:48 +0100

    Fixed Server Error Handling

--- backend/main.go
+++ backend/main.go
@@ -10,49 +10,40 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/404", handle404)
-	http.HandleFunc("/500", handle500)
-
 	// Serve static files (CSS, images, etc.) from the frontend directory
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/styles"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./frontend/images"))))
 
+	http.HandleFunc("/", handleNotFound)
+	http.HandleFunc("/500", handle500)
 
 	_, err := handlers.GetArtists()
-	
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    // for _, artist := range artists {
-    //     fmt.Printf("Artist ID: %d, Name: %s\n", artist.ID, artist.Members)
-    // }
-	// for _, artist := range artists {	
-	// 	printRelatedDatesLocations(artist.Relations)
-	// 	fmt.Println("")
-	// }
-	
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
 	port := "3000"
 	println("Server listening on port http://localhost:" + port)
 	http.ListenAndServe(":"+port, nil)
 }
 
-func printRelatedDatesLocations(url string){
+/*
+func printRelatedDatesLocations(url string) {
 	relations, err := handlers.GetRelations(url)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 	for location, relatedDatesLocations := range relations.DatesLocations {
-        fmt.Printf("location: %s\n", location)
-        fmt.Println("Related DatesLocations:")
-        for _, relatedDateL := range relatedDatesLocations {
-            fmt.Println(relatedDateL)
-        }
-    }
+		fmt.Printf("location: %s\n", location)
+		fmt.Println("Related DatesLocations:")
+		for _, relatedDateL := range relatedDatesLocations {
+			fmt.Println(relatedDateL)
+		}
+	}
 }
+*/
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	tmpl = filepath.Join("frontend", tmpl+".html")
@@ -68,24 +59,22 @@ func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	}
 }
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	// data := struct{}{} // Data for rendering, if needed
-	// artists, _ := handlers.GetArtists()
-	// renderTemplate(w, "index", artists)
+func handleNotFound(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		// Serve the index.html page for the root path
+		combinedData, err := handlers.GetArtistsWithRelations()
+		if err != nil {
+			fmt.Println("Error:", err) // Print the error for debugging
+			http.Error(w, "Failed to retrieve data", http.StatusInternalServerError)
+			return
+		}
 
-	combinedData, err := handlers.GetArtistsWithRelations()
-    if err != nil {
-        fmt.Println("Error:", err) // Print the error for debugging
-        http.Error(w, "Failed to retrieve data", http.StatusInternalServerError)
-        return
-    }
-
-    renderTemplate(w, "index", combinedData)
-}
-
-func handle404(w http.ResponseWriter, r *http.Request) {
-	data := struct{}{} // Data for rendering, if needed
-	renderTemplate(w, "404", data)
+		renderTemplate(w, "index", combinedData)
+	} else {
+		// Serve the 404.html page for other not-found routes
+		data := struct{}{} // Data for rendering, if needed
+		renderTemplate(w, "404", data)
+	}
 }
 
 func handle500(w http.ResponseWriter, r *http.Request) {
